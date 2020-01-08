81735e97e37aad06385a3ff651e2107c8bc6ccc9
abel <abel@gustavo.gustavo.cat>
2020-01-08 19:18:05 +0100

    ss

---
+++ app/src/main/java/edu/fje/dam2/abel/sopadelletres/Main2Activity.java
@@ -0,0 +1,13 @@
+package edu.fje.dam2.abel.sopadelletres;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
